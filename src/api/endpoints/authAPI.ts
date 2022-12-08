747ca087d1587d8613142430644b2f558cfbb321
Evgeniy <foksmix256@gmail.com>
2022-12-08 16:22:39 +0200

    trying deploy 3

--- src/api/endpoints/authAPI.ts
+++ src/api/endpoints/authAPI.ts
@@ -3,30 +3,31 @@ import instance from '..'
 import {
     IResponseUserData,
     IResponseAvatarData,
-    IResponseLogoutData
+    IResponseLogoutData,
 } from '../../models/responseModels'
 import { IRegisterDate } from '../../models/models'
 
-
-
 class AuthAPI {
     static registration = (values: IRegisterDate) => {
-        return instance.post<IResponseUserData>('/auth/register', values)
+        return instance.post<IResponseUserData>('/auth/register', values, {
+            withCredentials: true,
+        })
     }
     static login = (values: ILoginDate) => {
-        return instance.post<IResponseUserData>('/auth/login', values)
+        return instance.post<IResponseUserData>('/auth/login', values, {
+            withCredentials: true,
+        })
     }
     static logout = () => {
         return instance.post<IResponseLogoutData>('/auth/logout')
     }
     static refresh = () => {
-        return instance.get<IResponseUserData>('/auth/refresh')
+        return instance.get<IResponseUserData>('/auth/refresh', {
+            withCredentials: true,
+        })
     }
     static setAvatar = (file: FormData) => {
-        return instance.post<IResponseAvatarData>(
-            `/set-avatar`,
-            file
-        )
+        return instance.post<IResponseAvatarData>(`/set-avatar`, file)
     }
 }
 
